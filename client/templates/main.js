cc60d65768d04235d6065a89321810a5855bc043
Nicola Squartini <tensor5@gmail.com>
2017-09-17 11:15:15 +0200

    Fix ticket scanner starting on words selection

--- client/templates/main.js
+++ client/templates/main.js
@@ -2,7 +2,7 @@ import {FineRouter} from 'meteor/aadams:fine-router'
 import {Template} from 'meteor/templating'
 
 Template.main.events({
-  'click button': () => {
+  'click #scan': () => {
     FineRouter.go('scanTicket')
   }
 })

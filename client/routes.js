52950cf67796181e678ef9d94d232cc6dd35b001
gabrielem_ <coincrowd.it@gmail.com>
2017-09-17 11:30:41 +0200

    Add templates to routes

--- client/routes.js
+++ client/routes.js
@@ -2,16 +2,16 @@ import {FineBlazeLayout} from 'meteor/aadams:fine-blaze-layout'
 import {FineRouter} from 'meteor/aadams:fine-router'
 
 FineRouter.route('/', {
-  action: () => FineBlazeLayout.render('main'),
+  action: () => FineBlazeLayout.render('main', {content: "home"}),
   name: 'home'
 })
 
 FineRouter.route('/scanTicket', {
-  action: () => FineBlazeLayout.render('scanTicket'),
+  action: () => FineBlazeLayout.render('main', {content: "scanTicket"}),
   name: 'scanTicket'
 })
 
 FineRouter.route('/ticketInfo', {
-  action: () => FineBlazeLayout.render('ticketInfo'),
+  action: () => FineBlazeLayout.render('main', {content: "ticketInfo"}),
   name: 'ticketInfo'
 })

db8fa4e89c1fb8bcd7dcbf41418e7b75dea05d3b
gabrielem_ <coincrowd.it@gmail.com>
2017-09-17 12:07:19 +0200

    Add collection Tickets and insert method

--- client/templates/ticketInfo.js
+++ client/templates/ticketInfo.js
@@ -1,13 +1,42 @@
 import {Template} from 'meteor/templating'
 
-const ticket = {
-  from: 'Roma Termini',
-  to: 'Milano Centrale',
-  departsAt: '10:00',
-  arrivesAt: '12:45'
-}
+const ticket = [
+  {
+    from: 'Roma Termini',
+    to: 'Milano Centrale',
+    departsAt: '10:00',
+    arrivesAt: '12:45'
+  },
+  {
+    from: 'Milano Centrale',
+    to: 'Roma Termini',
+    departsAt: '18:10',
+    arrivesAt: '21:55'
+  }
+]
+
+Template.ticketInfo.onCreated(() => {
+  //setTitle()
+  var tickets = Tickets.find()
+  var count = tickets.count()
+  console.log("count",count);
+  var index = count
+  Tickets.insert(ticket[index])
+})
+
 
 Template.ticketInfo.helpers({
+
+  //TODO: return the last ticket
+  ticket: () => {
+    var tickets = Tickets.find()
+    var count = tickets.count()
+    var ticketsArray = tickets.fetch()
+    var ticket = ticketsArray[count]
+    return ticket
+  },
+
+
   from: () => ticket.from,
   to: () => ticket.to,
   departsAt: () => ticket.departsAt,

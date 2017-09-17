a84d3a753607478293e9af61970f1bd80a4054de
gabrielem_ <coincrowd.it@gmail.com>
2017-09-17 11:58:18 +0200

    Add css

--- client/templates/main.js
+++ client/templates/main.js
@@ -1,6 +1,23 @@
 import {FineRouter} from 'meteor/aadams:fine-router'
 import {Template} from 'meteor/templating'
 
+
+
+Template.main.onRendered(function() {
+    this.autorun(() => {
+      if ( !this.subscriptionsReady() ) {
+          return;
+      }
+      $(document).on('click','.navbar-collapse.in',function(e) {
+          if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+              $(this).collapse('hide');
+          }
+      });
+      
+      //new WOW().init()
+    })
+  })
+
 Template.main.events({
   'click #scan': () => {
     FineRouter.go('scanTicket')
